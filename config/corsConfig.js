a4b4290259732cf5363c0589dffaa3ada18c0918
jasper4545 <fernandezjasper463@gmail.com>
2023-08-27 10:12:20 +0800

    delete

--- config/corsConfig.js
+++
@@ -1,9 +0,0 @@
-
-const corsConfig = {
-  origin: (origin, callback)=>{
-    console.log(origin)
-    if(!origin || origin === "http://localhost:7700") callback(null, true)
-  }
-}
-
-module.exports = corsConfig;
--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-
-const logger = (req,res,next)=>{
-  const body = req.body
-  const log = `[ ${req.method} ]   ${req.url}   ${req.headers.origin || "android app"}   ${body || "no body" }`
-  console.log(log)
-  next()
-}
-
-module.exports = logger
